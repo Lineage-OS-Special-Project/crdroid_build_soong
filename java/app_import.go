ad385fce8909db1ae43c9381a1e8f71a006ab76d
Michael Bestas <mkbestas@gmail.com>
2025-03-10 17:18:55 +0200

    soong: Support installing apps to fdroid directory
    Change-Id: I5c1b49d41e78225999d2459ba0046c3307d6d123

--- java/app_import.go
+++ java/app_import.go
@@ -131,6 +131,9 @@ type AndroidAppImportProperties struct {
 	// need to either specify a specific certificate or be presigned.
 	Default_dev_cert *bool
 
+	// Specifies that this app should be installed to the fdroid directory.
+	Fdroid *bool
+
 	// Specifies that this app should be installed to the priv-app directory,
 	// where the system will grant it additional privileges not available to
 	// normal apps.
@@ -435,12 +438,14 @@ func (a *AndroidAppImport) generateAndroidBuildActions(ctx android.ModuleContext
 
 	var pathFragments []string
 	relInstallPath := String(a.properties.Relative_install_path)
-	if !proptools.Bool(a.properties.Install_path_skip_module_dir) {
+	if !proptools.Bool(a.properties.Install_path_skip_module_dir) && !proptools.Bool(a.properties.Fdroid) {
 		// Default add module name folder into install path.
 		relInstallPath = filepath.Join(relInstallPath, a.BaseModuleName())
 	}
 
-	if Bool(a.properties.Privileged) {
+	if Bool(a.properties.Fdroid) {
+		pathFragments = []string{"fdroid", relInstallPath}
+	} else if Bool(a.properties.Privileged) {
 		pathFragments = append([]string{"priv-app"}, relInstallPath)
 	} else if ctx.InstallInTestcases() {
 		pathFragments = []string{relInstallPath, ctx.DeviceConfig().DeviceArch()}
